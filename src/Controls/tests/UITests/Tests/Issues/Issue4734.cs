5c7ff39419231d4fc7ee1551d1f2c5afebe850ba
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-08-17 11:53:06 +0200

    Added UI Test

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue4734.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Maui.Appium;
+using NUnit.Framework;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue4734 : _IssuesUITest
+	{
+		public Issue4734(TestDevice device)
+		: base(device)
+		{ }
+
+		public override string Issue => "Gestures in Label Spans not working";
+
+		[Test]
+		public void Issue4734Test()
+		{
+			if (Device == TestDevice.Windows)
+			{
+				Assert.Ignore("This test is failing, likely due to product issue");
+			}
+			else
+			{
+				App.WaitForElement("WaitForStubControl");
+				App.Tap("TargetSpan");
+				var textAfterTap = App.Query("TapResultLabel").First().Text;
+				Assert.False(string.IsNullOrEmpty(textAfterTap));
+			}
+		}
+	}
+}

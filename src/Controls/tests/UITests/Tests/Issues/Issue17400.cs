4df9cfdaab3a38f2bcc71a78762049feed182bc9
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-09-26 16:58:17 +0200

    Adding test

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue17400.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Maui.Appium;
+using NUnit.Framework;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue17400 : _IssuesUITest
+	{
+		public Issue17400(TestDevice device)
+			: base(device)
+		{ }
+
+		public override string Issue => "CollectionView wrong Layout";
+
+		[Test]
+		public void Issue17400Test()
+		{
+			UITestContext.IgnoreIfPlatforms(new TestDevice[] { TestDevice.iOS, TestDevice.Android },
+				"Is a Windows issue; see https://github.com/dotnet/maui/issues/17400");
+
+			App.WaitForElement("WaitForStubControl");
+			App.Tap("UpdateBtn");
+			VerifyScreenshot();
+		}
+	}
+}
